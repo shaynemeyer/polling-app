a2997baa4767add2d99dfb10f100f939b0df8f12
Shayne Meyer <shayne.meyer@gmail.com>
2024-03-19 19:27:47 -0700

    added env fetch code

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
-
+from upstash_redis import Redis
+from dotenv import load_dotenv
+import os
 from app.models.Polls import Poll, PollCreate
+
+load_dotenv()
+
+REDIS_URL = os.getenv("REDIS_URL")
+REDIS_TOKEN = os.getenv("REDIS_TOKEN")
 
 app = FastAPI()
 
@@ -16,3 +23,18 @@ def create_poll(poll: PollCreate):
     # return Poll(title="Some placeholder title", options=["yes", "no", "maybe"])
     new_poll = poll.create_poll()
     return {"detail": "Poll successfully created", "poll_id": new_poll.poll_id}
+
+
+redis_client = Redis(url=REDIS_URL, token=REDIS_TOKEN)
+
+
+@app.post("/redis/save", tags=["throwaway"])
+def save_redis(id: str, name: str):
+    redis_client.set(id, name)
+    return {"status": "success"}
+
+
+@app.get("/redis/get/{id}", tags=["throwaway"])
+def get_redis(id: str):
+    name = redis_client.get(id)
+    return {"id": id, "name": name}
