1d4294645816c2c9c23e2bf15f2a6c001fb7f5fe
Shayne Meyer <shayne.meyer@gmail.com>
2024-03-19 20:25:07 -0700

    added application metadata

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from fastapi import FastAPI
 from app.api import polls
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Polls API",
+    description="A simple API to create and vote on polls",
+    version="0.1",
+    openapi_tags=[
+        {
+            "name": "polls",
+            "description": "Operations related creating and viewing polls",
+        }
+    ],
+)
 
 app.include_router(polls.router, prefix="/polls", tags=["polls"])
-
-
-@app.get("/test")
-def test():
-    return {"message": "Hello there"}
